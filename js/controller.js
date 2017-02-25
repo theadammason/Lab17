43a97276f8a88dcd58a6f001ff91ba43fd059185
theadammason <adam.mason86@gmail.com>
2017-02-25 16:40:48 -0500

    finished

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,17 @@
 var app = angular.module('myMod');
 
-app.controller('getReddit', function($scope, $http){
-
-  $scope.pulledData={};
-
-  function getData() {
-      $http({
-        url: 'https://www.reddit.com/r/awww/.json',
-        method: 'GET'
-      }).then(function success(response){
-        console.log(response);
-        $scope.pulledData = response;
-      }, function(error) {
-        console.log(error);
-      });
+app.controller('getReddit', function($scope, $http) {
+    function getData() {
+        $http({
+            url: 'https://www.reddit.com/r/awww/top/.json',
+            method: 'GET'
+        }).then(function success(response) {
+            $scope.pulledData = response;
+            console.log(response);
+            $scope.posts = $scope.pulledData.data.data.children;
+        }, function(error) {
+            console.log(error);
+        });
     };
-
-  return getData();
-
-
-
-
+  getData();
 });
